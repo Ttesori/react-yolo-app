794c2f7f0b6d5d137a6b990ec5c3f39207d43bd8
Toni Tesori <tt@tesoridesign.com>
2021-08-04 18:24:25 -0400

    Added placeholder tests

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,11 +2,27 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
-  test('renders App component', () => {
+  it('renders App component', () => {
     render(<App />);
+  });
+
+  it('increases round after play button is pressed', () => {
+    expect(true).toBe(false);
+  });
+
+  it('ends game after 5 rounds', () => {
+    expect(true).toBe(false);
+  });
+
+  it('ends game after points drops to zero', () => {
+    expect(true).toBe(false);
+  });
+
+  it('adds round info to history', () => {
+    expect(true).toBe(false);
+  });
+  it('adds score to scoreboard', () => {
+    expect(true).toBe(false);
   });
 });
 
-// test('', () => {
-
-// });
